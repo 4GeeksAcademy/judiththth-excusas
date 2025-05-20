8da0bc8e3146efe93e6bd909a04de343c79a4488
Judith Ramírez <judithramirezpachon@gmail.com>
2025-05-20 15:10:00 +0000

    excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  window.onload = function excuseGenerator(){
+  //partes excusas
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car', 'the house'];
+  let when = ['before the class.', 'when I was sleeping.', 'while I was exercising.', 'during my lunch.',];
+
+  //genera nº aleatorio
+  let random1 = Math.floor(Math.random() * 4);
+  let random2 = Math.floor(Math.random() * 4);
+  let random3 = Math.floor(Math.random() * 4);
+  let random4 = Math.floor(Math.random() * 4);
+
+  document.getElementById("excuse").innerHTML = who[random1] +" "+ action[random2] +" "+ what[random3] +" "+ when[random4];
+  console.log("Judith Ramírez");
 };
